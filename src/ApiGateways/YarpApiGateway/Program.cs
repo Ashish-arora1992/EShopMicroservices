044f35475d3122f6ecdd2e0d831415ac4b75dfad
Ashish arora <ashisharora1692@gmail.com>
2025-02-06 17:29:48 +0530

    deployed yarp proxy in docker

--- src/ApiGateways/YarpApiGateway/Program.cs
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,8 +1,21 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
-var app = builder.Build();
 
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddFixedWindowLimiter("fixed", opt =>
+    {
+        opt.PermitLimit = 4;
+        opt.Window = TimeSpan.FromSeconds(20);
+        //opt.QueueProcessingOrder = System.Threading.RateLimiting.QueueProcessingOrder.OldestFirst;
+       // opt.QueueLimit = 2;
+    });
+});
+var app = builder.Build();
+app.UseRateLimiter();
 app.MapReverseProxy();
 
 app.Run();
